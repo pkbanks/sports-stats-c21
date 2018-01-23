d0848e31501fd37ff673032ad77441d15059b4b8
PK Banks <pkbanks12@gmail.com>
2018-01-23 09:46:27 -0500

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :stats
-  resources :players
-  resources :teams
+  # resources :stats
+  # resources :players
+  # resources :teams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :teams do
+    resources :players, only: [:index, :new, :create]
+  end
+
+  resources :players, only: [:show, :edit, :update, :destroy]
+
 end
